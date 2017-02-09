dbc02dd8ec656089bff12656af6962ce28f3a491
vishala <vishu.kanashetti@gmail.com>
2017-02-09 15:37:05 +0530

    added one sop statement
    Change-Id: I0a4f91483d5b4abac8f1748f43fe3bcb845b9051

---
+++ aaa/src/aaa/A.java
@@ -0,0 +1,15 @@
+package aaa;
+
+public class A {
+
+	static{
+		System.out.println("hi");
+	}
+	public static void main(String[] args) {
+		
+			System.out.println("hello");
+			System.out.println("hi friends");
+		
+}
+}
+
